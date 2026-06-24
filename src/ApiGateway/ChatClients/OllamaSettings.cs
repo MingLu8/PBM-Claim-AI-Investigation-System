09b95f8ce8aca3dd88d443c60d471dda004f29b2
MingLu8 <ming.c.lu@gmail.com>
2026-06-24 09:01:29 -0500

    use small ollama model and memory is working.

--- src/ApiGateway/ChatClients/OllamaSettings.cs
+++ src/ApiGateway/ChatClients/OllamaSettings.cs
@@ -3,5 +3,5 @@ namespace ApiGateway.ChatClients;
 public class OllamaSettings
 {
     public string Endpoint { get; init; } = "http://localhost:11434";
-    public string Model { get; init; } = "llama4";
+    public string Model { get; init; } = "llama3.1:8b";
 }
